8a9c15ea98eca7f2baf4b9e2c9b30a89154ad688
Treasure-cd <treasureaj14@gmail.com>
2026-01-19 15:53:06 +0100

    Fixed vite config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 
 export default defineConfig({
+  base: '/',
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
